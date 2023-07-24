a153536ca37a3db9955bf889c2e6221370bab1ff
mouni396545 <229x5a05l1@gprec.ac.in>
2023-07-24 11:45:36 +0530

    First java programHelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+      public static void main(String[] args) {
+      //prints "Hello, World"to the terminal window.
+      Sysyem.out.println("Hello, World");
+      }
+ }
